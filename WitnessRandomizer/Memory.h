d5d5868cc4fb6ddff6ee73d9582a629b170ced64
jbzdarkid <jbzdarkid@gmail.com>
2018-10-21 16:58:03 -0700

    initial version, randomizing just tutorial

---
+++ WitnessRandomizer/Memory.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <windows.h>
+
+// https://github.com/erayarslan/WriteProcessMemory-Example
+// http://stackoverflow.com/q/32798185
+// http://stackoverflow.com/q/36018838
+// http://stackoverflow.com/q/1387064
+class Memory
+{
+public:
+	Memory(const std::string& processName);
+	~Memory();
+
+	template<class T>
+	std::vector<T> ReadData(const std::vector<int>& offsets, int numItems) {
+		std::vector<T> data;
+		data.resize(numItems);
+		if (!ReadProcessMemory(_handle, (LPVOID)ComputeOffset(offsets), &data[0], sizeof(T) * numItems, NULL)) {
+			ThrowError();
+		}
+		return data;
+	}
+
+	template <class T>
+	void WriteData(const std::vector<int>& offsets, const std::vector<T>& data) {
+		if (!WriteProcessMemory(_handle, (LPVOID)ComputeOffset(offsets), &data[0], sizeof(T) * data.size(), NULL)) {
+			ThrowError();
+		}
+	}
+	
+private:
+	void ThrowError();
+
+	uintptr_t ComputeOffset(std::vector<int> offsets);
+	uintptr_t _baseAddress;
+	HANDLE _handle;
+};
