f255be24d96352c43ec5197cf1bfd6a5a7bb47be
jbzdarkid <jbzdarkid@gmail.com>
2018-10-22 19:53:18 -0700

    Updates to the list of offsets -- being more explicit now

--- WitnessRandomizer/WitnessRandomizer.h
+++ WitnessRandomizer/WitnessRandomizer.h
@@ -1,13 +1,22 @@
 #pragma once
 
+int SWAP_TARGETS = 0x1;
+int SWAP_PATHWAYS = 0x2;
+int SWAP_STYLE = 0x40;
+int SWAP_COLORS = 0x4;
+int SWAP_TRACED = 0x80;
+
+
+int SWAP_SIZE = 0x8;
+
+
 class WitnessRandomizer {
 public:
-	int RANDOMIZE_TARGETS = 0x1;
-	int RANDOMIZE_DATA = 0x2;
-		
+
 	WitnessRandomizer() = default;
 
 	void Randomize(std::vector<int> panels, int flags);
+	void SwapPanels(int panel1, int panel2, int flags);
 
 private:
 	void SwapPanelData(int panel1, int panel2, int finalOffset, int dataSize);
