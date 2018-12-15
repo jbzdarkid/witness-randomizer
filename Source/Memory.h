1e7b51d0c775bd9ad518202473245d6aa7d15357
jbzdarkid <jbzdarkid@gmail.com>
2018-12-15 13:55:55 -0800

    Right version

--- Source/Memory.h
+++ Source/Memory.h
@@ -4,8 +4,8 @@
 #include <vector>
 #include <windows.h>
 
-#define GLOBALS 0x5B28C0
-// #define GLOBALS 0x62A080
+// #define GLOBALS 0x5B28C0
+#define GLOBALS 0x62A080
 
 // https://github.com/erayarslan/WriteProcessMemory-Example
 // http://stackoverflow.com/q/32798185
