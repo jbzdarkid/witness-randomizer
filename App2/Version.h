fa5c27c9b5991f2daf526eb5fdd3e41200afa63e
jbzdarkid <jbzdarkid@gmail.com>
2021-01-17 16:15:37 -0800

    Try adding app2

---
+++ App2/Version.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define TO_STRING2(s) L#s
+#define TO_STRING(s) TO_STRING2(s)
+
+#define MAJOR 0
+#define MINOR 0
+#define PATCH 1
+
+#define VERSION_STR     TO_STRING(MAJOR) L"." TO_STRING(MINOR) L"." TO_STRING(PATCH)
+#define VERSION			MAJOR, MINOR, PATCH
+
+#define PRODUCT_NAME L"Witness Challenge Randomizer"
+#define WINDOW_CLASS L"WitnessChallengeRandomizer"
